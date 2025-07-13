1c71c02c1817f0cc911e7f7fd44e435f72a36f41
= <=>
2025-07-13 13:35:00 -0700

    Strings 02 - includes y length

---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,16 @@
+const productos = "Monitor 20 pulgadas";
+console.log(productos);
+// Conocer la cantidad de letras de la cadena de texto
+console.log(productos.length);
+
+// Si la cadena tiene el texto
+// devuelve la posición
+console.log(productos.indexOf("Monitor"));
+console.log(productos.indexOf("pulgadas"));
+console.log(productos.indexOf("Tablet"));
+
+// Si la cadena tiene el texto
+// devuelve true o false
+console.log(productos.includes("Monitor"));
+console.log(productos.includes("pulgadas"));
+console.log(productos.includes("Tablet"));
