dca02d54deb93a70f81a7b544bf3d99ee6264182
= <=>
2025-07-13 12:34:46 -0700

    Variables 01 - forma general declaración

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,34 @@
+var producto = "Monitor de 24 pulgadas";
+console.log(producto);
+
+// Las variables se pueden reasignar
+producto = "Monitor de 19 pulgadas";
+console.log(producto);
+
+// JavaScript es un lenguaje de tipo dinámico,
+// no se especifica un tipo de dato
+producto = 20;
+console.log(producto);
+
+// Se pueden inicializar y asignarlo depués
+var disponible;
+disponible = true;
+
+disponible = false;
+
+//Inicializar múltiples variables
+var categoria = "Computadoras";
+var marca = "Marca famosa";
+var calificacion = 5;
+
+//Separar las variables con coma
+var categoria2 = "Computadoras",
+  marca2 = "Marca famosa",
+  calificacion2 = 5;
+
+// Camelcase para nombres de variables de varias palabras
+var nombreProducto;
+// Tipo serpiente o underscore
+var nombre_producto_categoria_precio;
+// PascalCase
+var Productos;
