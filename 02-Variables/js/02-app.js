a5d4e5614e88e56091d9c76bd99d77fc73949ae0
= <=>
2025-07-13 12:41:48 -0700

    Variables 02 - Declaración de variables con let

---
+++ 02-Variables/js/02-app.js
@@ -0,0 +1,13 @@
+let productos = "Tablet";
+
+// Reasignar el valor
+productos = "Monitor";
+productos = 20;
+productos = true;
+productos = null;
+
+console.log(productos);
+
+//Declarar variables sin inicializarlas
+let precios;
+console.log(precios);
