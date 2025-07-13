f8195b4cadeb576a458d00dadc07a8ae3d06e572
= <=>
2025-07-13 12:47:47 -0700

    Variables 03 - uso de constantes

---
+++ 02-Variables/js/03-app.js
@@ -0,0 +1,12 @@
+const prod = "Tablet";
+
+// No se pueden reasignar
+// prod = "Monitor";
+
+console.log(prod);
+
+// Las constantes se deben inicializar
+// Esto va a marcar error
+// const precios2;
+const precios2 = 100.0;
+console.log(precios2);
