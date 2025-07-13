b2146361b7fb611e340dceddcb1745952600f335
= <=>
2025-07-12 23:14:26 -0700

    Uso de comentarios

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,3 +1,9 @@
 // alert("Hola Mundo");
+// Comentario de una línea
+/*
+    Comentario de múltiples líneas
+*/ 
+// Pregunta al usuario su nombre
 const nombre = prompt("¿Cuál es tu nombre?");
+// Documentar una línea de código
 document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo JavaScript moderno` 
