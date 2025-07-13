862dcf1212132812e3e0936216d283505323aca4
= <=>
2025-07-13 15:04:19 -0700

    Strings 06 - Convertir a mayúsculas y minúsculas

---
+++ 03-Strings/js/07-app.js
@@ -0,0 +1,10 @@
+const productos05 = "Monitor 20 pulgadas";
+// Convertir a mayúsculas
+console.log(productos05.toLocaleUpperCase());
+// Convertir a minúsculas
+console.log(productos05.toLocaleLowerCase());
+
+// Convertir de número a strings
+const precios07 = 300;
+console.log(precios07);
+console.log(precios07.toString());
