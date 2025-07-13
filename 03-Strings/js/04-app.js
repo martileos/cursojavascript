8ddc741211c8afd641ea7dc60063ff4fb61b7738
= <=>
2025-07-13 13:46:45 -0700

    Strings 04 - Eliminar espacios en blanco

---
+++ 03-Strings/js/04-app.js
@@ -0,0 +1,15 @@
+const productos02 = "            Monitor 20 pulgadas             ";
+console.log(productos02);
+console.log(productos02.length);
+
+// Eliminar espacios del inicio
+console.log(productos02.trimStart());
+console.log(productos02.trimStart().length);
+
+// Eliminar espacios del final
+console.log(productos02.trimEnd());
+console.log(productos02.trimEnd().length);
+
+// Eliminar todos los espacios
+console.log(productos02.trimStart().trimEnd());
+console.log(productos02.trimStart().trimEnd().length);
