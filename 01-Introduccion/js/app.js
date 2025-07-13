fbb30d5faa08c3ad6707f6eee037f1a1531a745c
= <=>
2025-07-12 23:22:53 -0700

    Uso de la consola del navegador

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -7,3 +7,24 @@
 const nombre = prompt("¿Cuál es tu nombre?");
 // Documentar una línea de código
 document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo JavaScript moderno` 
+
+// Tipos de mensajes para la consola del navegador
+console.log("Mensaje en consola de texto sencillo")
+// Mensaje en tabla
+console.table([1,2,3,4,5])
+// Mensaje de error
+console.error("Este es un error hacia la consola")
+// Mensaje de advertencia
+console.warn("Advertencia en consola")
+// Console.time inicia un contador de tiempo de ejecución de código
+// se detiene hasta que encuentra un console.timeEnd y da el resultado
+// con la cantidad de tiempo de ejeción del código entre time y timeEnd.
+console.time("Hola")
+console.log("Mensajes hacia la consola para ver cuanto tarda");
+console.log("Mensajes hacia la consola para ver cuanto tarda");
+console.log("Mensajes hacia la consola para ver cuanto tarda");
+console.log("Mensajes hacia la consola para ver cuanto tarda");
+console.log("Mensajes hacia la consola para ver cuanto tarda");
+console.log("Mensajes hacia la consola para ver cuanto tarda");
+console.log("Mensajes hacia la consola para ver cuanto tarda");
+console.timeEnd("Hola")
