9f8afcc31a2485d3c5af6a4fc9b622505c29052b
= <=>
2025-07-13 19:01:19 -0700

    Números 03 - Objeto Math

---
+++ 04-Numeros/js/03-app.js
@@ -0,0 +1,44 @@
+let res1;
+
+// PI
+res1 = Math.PI;
+
+// Redondear
+res1 = Math.round(2.8);
+console.log(res1);
+res1 = Math.round(2.2);
+console.log(res1);
+res1 = Math.round(2.6);
+console.log(res1);
+res1 = Math.round(2.5);
+console.log(res1);
+res1 = Math.round(2.4);
+console.log(res1);
+
+// Redondear hacia arriba
+res1 = Math.ceil(2.1);
+console.log(res1);
+
+// Valor absoluto
+res1 = Math.abs(-500);
+console.log(res1);
+
+// Potencia
+res1 = Math.pow(2, 4);
+console.log(res1);
+
+// Mínimo
+res1 = Math.min(3, 5, 1, 12, -3);
+console.log(res1);
+
+// Máximo
+res1 = Math.max(3, 5, 1, 12, -3);
+console.log(res1);
+
+// Aleatorio
+res1 = Math.random() * 20;
+console.log(res1);
+
+// Aleatorio dentro de un rango
+res1 = Math.floor(Math.random() * 30);
+console.log(res1);
