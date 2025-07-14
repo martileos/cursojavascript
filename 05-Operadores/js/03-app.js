ad0b9166390781de7a5044f7cd935438683bba59
= <=>
2025-07-14 16:34:55 -0700

    Operadores 03 - comparando undefined y null

---
+++ 05-Operadores/js/03-app.js
@@ -0,0 +1,14 @@
+// Undefined
+let n1;
+console.log(n1);
+console.log(typeof n1);
+
+// Null
+let n2 = null;
+console.log(n2);
+console.log(typeof n2);
+
+// Los pone como true
+console.log(n1 == n2);
+// Los pone como false
+console.log(n1 === n2);
