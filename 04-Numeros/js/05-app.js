3e90c33c31b2eae75f774cce74c4b9431b0f4ed0
= <=>
2025-07-13 21:33:07 -0700

    Números 05 - Incrementos y decrementos

---
+++ 04-Numeros/js/05-app.js
@@ -0,0 +1,21 @@
+let puntaje = 5;
+console.log(puntaje);
+// Imprime y luego incrementa
+console.log(puntaje++);
+// Aquí si imprime el incremento
+console.log(puntaje);
+// Incrementa y luego imprime
+console.log(++puntaje);
+
+// Imprime y luego decrementa
+console.log(puntaje--);
+// Aquí si imprime el decremento
+console.log(puntaje);
+// Decrementa y luego imprime
+console.log(--puntaje);
+
+// Abreviaturas
+puntaje += 3;
+console.log(puntaje);
+puntaje -= 3;
+console.log(puntaje);
