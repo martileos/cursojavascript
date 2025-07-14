1ac1597aee1aec988e7cb4d1fd2d05046180530b
= <=>
2025-07-14 16:50:22 -0700

    Booleans 03 - buenas prácticas de comparación

---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,16 @@
+const autenticado = true;
+
+if (autenticado === true) {
+  console.log("Si puedes ver Netflix");
+} else {
+  console.log("No puedes verlo");
+}
+
+if (autenticado) {
+  console.log("Si puedes ver Netflix");
+} else {
+  console.log("No puedes verlo");
+}
+
+// Operador ternario
+console.log(autenticado ? "Si está autenticado" : "No está autenticado");
