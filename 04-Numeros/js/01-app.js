e59522278fd3b39e5837b24fc7a28fc488e19428
= <=>
2025-07-13 18:16:10 -0700

    Números 01 - Crear números en JS

---
+++ 04-Numeros/js/01-app.js
@@ -0,0 +1,21 @@
+// Creación de números
+const numero1 = 20;
+// Los números se imprimen en consola en color azul
+console.log(numero1);
+const numero2 = "20";
+// Los números que son string se imprimen en consola en color negro
+console.log(numero2);
+
+const n1 = 30;
+const n2 = 20;
+const n3 = 20.2;
+const n4 = 0.202;
+const n5 = -20;
+console.log(n1);
+console.log(n2);
+console.log(n3);
+console.log(n4);
+console.log(n5);
+
+const n6 = new Number(80);
+console.log(n6);
