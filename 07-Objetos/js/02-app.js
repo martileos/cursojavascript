1ca0a247a1527a8f05df5bb02ed501453a069a25
= <=>
2025-07-14 22:14:31 -0700

    Objetos 02 - Acceso a valores del objeto

---
+++ 07-Objetos/js/02-app.js
@@ -0,0 +1,12 @@
+const producto = {
+  nombre: "Monitor de 20 pulgadas",
+  precio: 300,
+  disponible: true,
+};
+console.log(producto);
+// Los objetos tienen sintáxis de punto
+console.log(producto.nombre);
+console.log(producto.precio);
+console.log(producto.disponible);
+// Como si fuera un arreglo
+console.log(producto["nombre"]);
