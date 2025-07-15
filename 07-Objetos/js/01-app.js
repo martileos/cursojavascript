96b4c3801c2294e40cbad2d80d85c5bdfaeae535
= <=>
2025-07-14 21:58:33 -0700

    Objetos 01 - Crear objetos en JS

---
+++ 07-Objetos/js/01-app.js
@@ -0,0 +1,13 @@
+const nombre = "Monitor de 20 pulgadas";
+const precio = 300;
+const disponible = true;
+
+// Un objeto agrupa todo en una sola variable
+// Tiene propiedades o llave del objeto
+const producto = {
+  nombre: "Monitor de 20 pulgadas",
+  precio: 300,
+  disponible: true,
+};
+
+console.log(producto);
