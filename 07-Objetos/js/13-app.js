8c305c515142ae6abdce7a6c7e6a48d3c3f44906
= <=>
2025-07-15 12:25:56 -0700

    Objetos 13 - Keys, values, entries

---
+++ 07-Objetos/js/13-app.js
@@ -0,0 +1,14 @@
+// Keys, values, entries
+
+const producto = {
+  nombre: "Monitor de 20 pulgadas",
+  precio: 300,
+  disponible: true,
+};
+
+// Atributos
+console.log(Object.keys(producto));
+// Valores
+console.log(Object.values(producto));
+// Las dos cosas
+console.log(Object.entries(producto));
